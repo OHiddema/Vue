5b6d13e6d024ed0037017a00946e5433a40a0ed7
Oege Hiddema <oegehiddema@gmail.com>
2019-07-05 14:04:48 +0200

    Add an onSale property to the products data

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var myViewInstance = new Vue ({
       number: 'Socks',
       image: 'assets/vmSocks-green-onWhite.jpg',
       link: 'https://www.google.com',
-      inventory: 0
+      inventory: 0,
+      onSale: true
    }
 })
